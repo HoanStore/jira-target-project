0c53a34451b1c0f735d9b8a8d1f8c443663487d7
조근완 <kwjo@telcoware.com>
2026-04-20 14:23:00 +0900

    Allow public access to landing page

--- src/main/java/io/spring/api/security/WebSecurityConfig.java
+++ src/main/java/io/spring/api/security/WebSecurityConfig.java
@@ -48,6 +48,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .antMatchers(HttpMethod.OPTIONS)
         .permitAll()
+        .antMatchers("/", "/index.html", "/static/**", "/css/**", "/js/**", "/images/**")
+        .permitAll()
         .antMatchers("/graphiql")
         .permitAll()
         .antMatchers("/graphql")
